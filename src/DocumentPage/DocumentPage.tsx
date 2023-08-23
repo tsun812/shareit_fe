d0accc41477f9c95d97d5bc93fb5037e5ee66dd8
tsun812 <tsun812@gmail.com>
2023-08-22 18:09:59 -0700

    fix layout

--- src/DocumentPage/DocumentPage.tsx
+++ src/DocumentPage/DocumentPage.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import TextEditor from '../Editor/TextEditor'
 import Preview from '../Preview/Preview'
 import { useState } from 'react'
-
+import './DocumentPage.css'
 const DocumentPage = () => {
   const [currentText, setCurrentText] = useState<string>('')
   return (
     <div className='container'>
-    <TextEditor setCurrentText={setCurrentText} />
-    <Preview currentText={currentText} />
+      <TextEditor setCurrentText={setCurrentText} />
+      <Preview currentText={currentText} />
   </div>
   )
 }
